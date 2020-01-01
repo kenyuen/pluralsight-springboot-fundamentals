839f41342643b075d491a916748042362d1a31f8
Kenneth Yuen <kenneth.x.yuen@gmail.com>
2020-01-01 13:34:49 -0600

    Updated to include Thymeleaf, web and services: MVC components.

---
+++ src/main/java/com/pluralsight/fundamentals/service/ApplicationService.java
@@ -0,0 +1,10 @@
+package com.pluralsight.fundamentals.service;
+
+import com.pluralsight.fundamentals.entity.Application;
+
+public interface ApplicationService {
+    Iterable<Application> listApplications();
+}
+
+
+
---
+++ src/main/java/com/pluralsight/fundamentals/service/ReleaseService.java
@@ -0,0 +1,10 @@
+package com.pluralsight.fundamentals.service;
+
+import com.pluralsight.fundamentals.entity.Release;
+
+public interface ReleaseService {
+    Iterable<Release> listReleases();
+}
+
+
+
